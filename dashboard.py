f30dbaafa81832e25027d38384e3fcd71a8f1203
Vasanthkumar7483 <Vasanthonline.in@gmail.com>
2025-04-15 10:01:18 +1000

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('Login', location='main')
 
 if authentication_status == False:
     st.error('Username/password is incorrect')
